7070d0db2501699907ebb8cd38395c2c2666e8fb
skyward <skyward@txzing.com>
2020-08-13 10:30:07 +0800

    init commit

---
+++ js/background.js
@@ -0,0 +1,39 @@
+/**
+ * 复制内容到粘贴板
+ * content : 需要复制的内容
+ * message : 复制完后的提示，不传则默认提示"复制成功"
+ */
+
+function copyToClip(content, ) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", content);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+}
+
+
+function isEmpty(obj) {
+    if (typeof obj == "undefined" || obj == null || obj == "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+chrome.contextMenus.create({
+    title: '快速替换github.com',
+    contexts: ['selection', 'link'],
+    onclick: function(params) {
+        // 注意不能使用location.href，因为location是属于background的window对象
+        if (!isEmpty(params.selectionText)) {
+            copyToClip(params.selectionText.replace("github.com", "git.sdut.me"))
+        }
+        if (!isEmpty(params.linkUrl)) {
+            copyToClip(params.linkUrl.replace("github.com", "git.sdut.me"))
+        }
+
+    }
+});
